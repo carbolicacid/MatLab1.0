b005f15f011a2fc0a317ef4400004a5d49421cb1
Rafayet Ahammad <155757421+carbolicacid@users.noreply.github.com>
2025-09-23 01:26:43 +0600

    Update problem02.m

--- RandomProblem/problem02.m
+++ RandomProblem/problem02.m
@@ -5,17 +5,21 @@ h=0.01;
 x=10:0.1:22;
 y= @(x) 3*x.^3+2*x.^2-6*x+7; 
 for i=2:length(x)-1
-    dv(i-1)= (y(x(i+1))-y(x(i-1)))/2*h;
+    v(i-1)= (y(x(i+1))-y(x(i-1)))/(2*h);
 end
 xv=x(2:length(x)-1);
-for i=2:length(x)-1
-    da(i-1)= (y(x(i+1))-y(x(i-1)))/2*h;
+for i=2:length(xv)-1
+    a(i-1) = (v(i+1) - v(i-1)) / (2*h);
+
 end
-xa=x(2:length(x)-1);
+xa=x(2:length(xv)-1);
 subplot 311
 plot (x,y(x))
 subplot 312
-plot (xv,dv)
+plot (xv,v)
 subplot 313
-plot(xa,da)
+plot(xa,a)
+
+
+
 
